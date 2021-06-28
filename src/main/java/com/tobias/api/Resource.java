da86d931447009f459a0c27bf77373bd75bb6f91
tobi <tobi.klatt@web.de>
2021-06-28 11:53:17 +0200

    Add resource representation
    Very Basic object to represent the input and calculated output. This 
    object will serve as the returned payload for the API GET request on 
    helloapi/fibonacci.

---
+++ src/main/java/com/tobias/api/Resource.java
@@ -0,0 +1,19 @@
+package com.tobias.api;
+
+public class Resource {
+    private int input;
+    private int output;
+
+    public Resource(int input, int output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public int getInput() {
+        return input;
+    }
+
+    public int getOutput() {
+        return output;
+    }
+}
